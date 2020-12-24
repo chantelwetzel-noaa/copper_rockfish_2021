e689ac1a182232a77767c3576aee452976b019a3
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-12-24 10:27:03 -0800

    additional model runs

--- code/_oregon_copper_runs.R
+++ code/_oregon_copper_runs.R
@@ -6,7 +6,6 @@ wd = "N:/Assessments/CurrentAssessments/DataModerate_2021/copper_rockfish/models
 wd = "C:/Assessments/2021/copper_rockfish_2021/models/or"
 
 model = "0.1.2_dw_francis"
-
 base = SS_output(file.path(wd, model))
 SS_plots(base)
 
@@ -71,11 +70,153 @@ SS_plots(base)
 # NLL = 242.9, R0 = 2.4, depl 2021 = 0.27
 # These changes to the selex pars fixed the gradient issue with p3
 
+# Add Data: 1) released fish (ghost fleet) and 2) ages (lambda = 0)
+model = "3.0.0_data_released_len_and_ages"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+
+model = "3.0.1_data_ages_lambda=1"
+ages = SS_output(file.path(wd, model))
+SS_plots(base)
+
+modelnames <- c("Lengths Only", "+ Ages")
+mysummary <- SSsummarize(list(base,  ages))
+
+SSplotComparisons(mysummary, 
+				  legendlabels = modelnames, 
+				  plotdir = file.path(wd, "_plots"),
+				  pdf = TRUE)
+
+# Estimate selex params p1-p4 for both fleets (with rec block)
+model = "3.1_selex_est_all"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 242.9, R0 = 2.4
+# Slight difference in estimated p2 and p4 but no change in fit or p1 and p3
+
+# Update p2 and p4 fixed values based on those estimated above.
+# Allow for dome shaped selectivity for both commercial and rec fleets
+# The dome applies to both periods of the rec selex
+model = "3.2_selex_domed"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 233.452, Com NLL = 128.565 (-2.2), Rec NLL = 98.6 (-5.6 LL from asymptotic)
+# R0 = 2.53, depl 2021 = 0.35
+
+# Fix recent rec selex asmptotic but allow for a dome in historical period
+model = "3.3_selex_domed_block_only"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 242.5, Rec LL = 104.2
+# R0 = 2.41
+
+# Fix block rec selex asmptotic but allow for a dome in recent years
+model = "3.4_selex_domed_rec_recent"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 231.5 (Rec LL = 94.6 -9 units from asymptotic)
+# R0 = 2.75 (vs. 2.4 asymptotic ), depl 2021 = 0.46 (vs. 0.27 asymptotic)
+
+# Remove rec block and fix asymptotic
+model = "3.5_selex_no_blocks_asymptotic"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 295.7, R0 = 2.4, depl 2021 = 0.33
+
+# Remove rec block and let estimate dome or asymptotic
+model = "3.6_selex_no_blocks_est_dome"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# Estimate dome selex for the rec fleet, clear residual pattern
+# NLL = 278.2, R0 = 3.7, depl 2021 = 0.82
+
+# Move forward with rec block selex with dome in recent years:
+# Update all the biology parameters
+model = "4.0_bio_update"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 231.5, R0 = 2.75, depl 2021 = 0.46
+
+# Sigma R reduced to 0.50 - estimate the hessian
+model = "4.1_bio_sigmaR_hessian"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# Now the recommended sigma R is 0.60...
+
+# SigmaR = 0.90 - start early devs 1900 - estimate the hessian
+model = "4.2_bio_sigmaR_90_hessian"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 231.9
+# Now recommending sigmaR = 0.688, poor gradient on 1980-82 rec devs
+# Sigma R = 0.70 -- NLL = 229.7
+# Now recommending simgaR = 0.77, gradient is fine
+# Sigma R = 0.80 -- NLL = 228.8
+# Sigma R = 0.90 -- NLL = 228.2 (recommending 0.92), R0 = 2.7
+# Moving forward with sigma R = 0.90
+
+model = "5.0_dw_francis"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 249.5, R0 = 2.71, depl 2021 =  0.42
+
+model = "5.1_dw_mi"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 293.7, R0 = 2.73, depl 2021 = 0.43
+
+model = "5.2_dw_dirichlet"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# R0 blows up 18.96 and commercial dirichlet hits upper bound
 
 
+# MI DW & SigmaR = 0.60
+model = "5.3_dw_mi_sigmar_60"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 295.98, R0 = 4.78, depl 2021 = 0.905
+# Did not expect things to change so dramatically since francis and mi dw were similar.
 
+# Need to decide on the input sigmaR and dw 
+# 	- MI results in less extreme rec devs when sigma R = 0.90
+#	- MI with sigma R = 0.60 - the model ends up in a very different place
+#	- Francis with sigma R = 0.90 vs. sigma R = 0.60 are similar but better NLL with sigma R = 0.90 by ~ 3 units
+#	- There seems to be some tension between commercial and rec lengths in terms of R0 which dw highlights
 
+#===========================================
+# Sensitivities
+#===========================================
+model = "4.1_bio_sigmaR_hessian"
+sigma60 = SS_output(file.path(wd, model))
 
+model = "4.2_bio_sigmaR_90_hessian"
+sigma90 = SS_output(file.path(wd, model))
+
+model = "5.0_dw_francis"
+francis = SS_output(file.path(wd, model))
+
+model = "5.1_dw_mi"
+mi = SS_output(file.path(wd, model))
+
+model = "5.3_dw_francis_use_ages"
+ages = SS_output(file.path(wd, model))
+
+modelnames <- c("SigmaR = 0.60", "SigmaR = 0.90", "Francis", "MI", "Francis w/ Ages")
+mysummary  <- SSsummarize(list(sigma60, sigma90, francis, mi, ages))
+SSplotComparisons(mysummary, 
+				  filenameprefix = "4.1_5.0_sensitivities",
+				  legendlabels = modelnames, 
+				  plotdir = file.path(wd, "_plots"),
+				  pdf = TRUE)
+
+# Remove/Add Blocks
+# Rec selectivity shape (domed vs. asymptotic)
+# No rec devs
+# Smaller sigma
+# Data weighting
+# Platoons
+# Use age data
 
 
 
--- code/_wa_copper_runs.R
+++ code/_wa_copper_runs.R
@@ -125,7 +125,117 @@ SS_plots(base)
 # +7 NLL when the block is removed
 
 
+# Reset to model 5.1.1_selex_cv_fixed
+# Remove data-weighting
+model = "6.1_bio_update_remove_dw"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 1132.25, R0 = 2.17
+# Length fits look crappy...
 
 
+# Estimate the selectivity parameters
+model = "6.2_selex_est_all"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# Fixed Asymptotic Selex: NLL = 1132.24, R0 = 2.17
+# Allow for dome-shaped selex: with domed-selex the model gets the best fit
+# with a really high R0.
+
+# Let's do some tests with age data
+# Full lambdas and estimating rec devs
+model = "6.3_recdevs_ghost_ages"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# Mean age and length of fish have increased in recent years which leads the model to 
+# infer that recruitment has been bad in recent years resulting in a sharp downturn
+# in stock status.
+# Estimating recdevs results in a drastic shift in stock status and scale,
+# along with a strong rightward shift in selectivity (P1 ~45 vs ~37 without).
+
+
+# Completely exploratory
+# No recdevs but a block for 2010-2019 which could be used to fit the 
+# shift in means lengths for the period
+model = "6.4_selex_block_2010_nodevs"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 894.435, R0 = 1.83
+# The fit to the length data  is considerably better but there is no 
+# explanation for this shift in selectivity. However, the stock scale is
+# much smaller and the status is much lower.
+
+# Remove the selectivity blocks & 
+# Re-evaluate the growth pattern approach
+model = "6.5_bio_growth_patterns"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 1105.53, R0 = 2.286, depl 2021 = 0.50
+# Dramatically improved the length LL but not entirely clear why
+# The R0 increases a bit and the depl is slightly greater across time.
+
+model = "6.6_bio_growth_patterns_w_devs"
+devs = SS_output(file.path(wd, model))
+SS_plots(devs)
+
+modelnames <- c("One Platoon", "Five Platoons", "Five Platoons w/ Devs")
+mysummary <- SSsummarize(list(base,  alt_platoons, devs))
+SSplotComparisons(mysummary, 
+				  legendlabels = modelnames, 
+				  plotdir = file.path(wd, "_plots"),
+				  pdf = TRUE)
+
+# Move forward with model with 1 platoon and no rec devs:
+model = "7.0_base"
+base = SS_output(file.path(wd, model))
+SS_plots(base)
+# NLL = 1132.24
+
+model = "7.1_dw_dirichlet"
+dirichlet = SS_output(file.path(wd, model))
+SS_plots(dirichlet)
+
+model = "7.2_dw_MI"
+mi = SS_output(file.path(wd, model))
+SS_plots(mi)
+
+model = "7.3_dw_francis"
+francis = SS_output(file.path(wd, model))
+SS_plots(francis)
+
+modelnames <- c("Base", "Dirichlet", "MI", "Francis")
+mysummary <- SSsummarize(list(base,  dirichlet, mi, francis))
+SSplotComparisons(mysummary, 
+				  legendlabels = modelnames, 
+				  plotdir = file.path(wd, "_plots"),
+				  pdf = TRUE)
+
+# Input all lengths as unsexed in order to better visualize the fits
+# No data weighting applied
+model = "7.4_lengths_combined"
+all_lengths = SS_output(file.path(wd, model))
+SS_plots(all_lengths)
+# NLL = 799.667, R0 = 2.17, depl 2021 = 0.45
+# I think the likelihood change is due to how input sample sizes are
+# entered between unsexed and sexed lengths. The model results are 
+# very similar.
+
+# Start with the 7.0_base model
+# Add 2 blocks (3 total selex periods) based on residual patterns
+model = "7.5_selex_3_blocks"
+blocks = SS_output(file.path(wd, model))
+SS_plots(blocks)
+# NLL = 823.2, R0 = 1.80, depl 2021 = 0.157
+# Although NLL decreased the mean lengths still are not fit well and the
+# aggregated length fit not dramatically improved.
+SS_plots(all_lengths)
+
+
+
+#===================================================================
+# Sensitivities to Run for Document
+#	- Recruitment deviations estimated
+#	- Block in selectivity from 2010-2019
+#
 
 
