2827ce55591c49aa11445af7785e7ed4db719493
Chantel Wetzel <chantel.wetzel@noaa.gov>
2021-01-08 05:45:35 -0800

    s ca sensitivities

---
+++ code/sensitivities_sca.R
@@ -0,0 +1,161 @@
+########################################################
+# Create Sensitivity table for Copper Rockfish 2021
+#   written by : Chantel Wetzel
+########################################################
+
+#library(r4ss)
+#devtools::install_github("r4ss/r4ss", ref = "development")
+devtools::load_all("C:/Users/Chantel.Wetzel/Documents/GitHub/r4ss")
+
+
+###################################################################
+# South of Pt Conception
+###################################################################
+area = "ca_s_pt_c"
+base_model = "10.0_base"
+
+wd = file.path("C:/Assessments/2021/copper_rockfish_2021/models", 
+          area, "_sensitivities")
+setwd(wd)
+out.dir = wd
+
+base_loc = file.path("C:/Assessments/2021/copper_rockfish_2021/models",
+           area, base_model)
+
+sens_list = c("no_recdevs", #1
+              "main_devs_earlier", #2
+              "dirichlet", #3
+              "mi", #4
+              "hkl_dome", #5
+              "add_com_data", #6
+              "add_com_data_main_devs_earlier", #7
+              "rm_hkl", #8
+              "dw_rec_data") #9
+
+model.list <- paste0(base_model, "_", sens_list)
+
+#out.list = NULL	
+base   = SS_output( base_loc, printstats = FALSE, verbose = FALSE) 
+#for (a in 2:length(model.list)){
+#    out = SS_output( paste0(wd, model.list[a]), printstats = FALSE)
+#    assign(paste0("sen_", a-1), out)
+#    out.list = c(out.list, paste0("sen_", a-1))
+#}
+sens_1  = SS_output( file.path(wd, model.list[1]), printstats = FALSE, verbose = FALSE, covar = FALSE) 
+sens_2  = SS_output( file.path(wd, model.list[2]), printstats = FALSE, verbose = FALSE, covar = FALSE) 
+sens_3  = SS_output( file.path(wd, model.list[3]), printstats = FALSE, verbose = FALSE, covar = FALSE)
+sens_4  = SS_output( file.path(wd, model.list[4]), printstats = FALSE, verbose = FALSE, covar = FALSE)
+sens_5  = SS_output( file.path(wd, model.list[5]), printstats = FALSE, verbose = FALSE, covar = FALSE)
+sens_6  = SS_output( file.path(wd, model.list[6]), printstats = FALSE, verbose = FALSE, covar = FALSE)
+sens_7  = SS_output( file.path(wd, model.list[7]), printstats = FALSE, verbose = FALSE, covar = FALSE)
+sens_8  = SS_output( file.path(wd, model.list[8]), printstats = FALSE, verbose = FALSE, covar = FALSE)
+sens_9  = SS_output( file.path(wd, model.list[9]), printstats = FALSE, verbose = FALSE, covar = FALSE)
+
+modelnames <- c("Base Model",
+                "No Rec. Devs.", 
+                "Start Main Devs. Earlier", 
+                "Add Early Comm. Data",
+                "Add Early Comm. Data & Main Devs. Earlier") 
+
+x <- SSsummarize(list(base, sens_1, sens_2, sens_6, sens_7))
+
+SSplotComparisons(x, endyrvec = 2021, 
+                  legendlabels = modelnames, 
+                  plotdir = file.path(getwd(), '_plots'), 
+                  legendloc = "topright", 
+                  filenameprefix = paste0(base_model, "_Devs_"),
+                  pdf = TRUE)
+
+modelnames <- c("Base Model",
+                 "Dirichlet DW",
+                 "MI DW",
+                 "Rec Length Lambda = 0.1",
+                 "HKL Domed",
+                 "Remove HKL Data") 
+
+x <- SSsummarize(list(base, sens_3, sens_4, sens_9, sens_5, sens_8))
+
+SSplotComparisons(x, endyrvec = 2021, 
+                  legendlabels = modelnames, 
+                  plotdir = file.path(getwd(), '_plots'), 
+                  legendloc = "topright", 
+                  filenameprefix = paste0(base_model, "_DW_"),
+                  pdf = TRUE)
+
+###################################################################################
+# Create a Table of Results
+###################################################################################
+
+ii = 1:length(modelnames)
+n = length(modelnames)
+out<- matrix(NA, 24, max(ii))
+
+out = rbind(
+            as.numeric(x$likelihoods[x$likelihoods$Label == "TOTAL",1:n]), 
+            as.numeric(x$likelihoods[x$likelihoods$Label == "Survey",1:n]), 
+            as.numeric(x$likelihoods[x$likelihoods$Label == "Length_comp",1:n]),
+            as.numeric(x$likelihoods[x$likelihoods$Label == "Age_comp",1:n]), 
+            as.numeric(x$likelihoods[x$likelihoods$Label == "Recruitment",1:n]), 
+            as.numeric(x$likelihoods[x$likelihoods$Label == "Forecast_Recruitment",1:n]),
+            as.numeric(x$likelihoods[x$likelihoods$Label == "Parm_priors",1:n]),
+            as.numeric(x$pars[x$pars$Label == "SR_LN(R0)", 1:n]), 
+            as.numeric(x$SpawnBio[x$SpawnBio$Label == "SSB_Virgin", 1:n]),
+            as.numeric(x$SpawnBio[x$SpawnBio$Label == "SSB_2021", 1:n]),
+            as.numeric(x$Bratio[x$Bratio$Label == "Bratio_2021", 1:n]), 
+            as.numeric(x$quants[x$quants$Label == "Dead_Catch_SPR", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "SR_BH_steep", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "NatM_p_1_Fem_GP_1", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "L_at_Amin_Fem_GP_1", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "L_at_Amax_Fem_GP_1", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "VonBert_K_Fem_GP_1", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "CV_young_Fem_GP_1", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "CV_old_Fem_GP_1", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "NatM_p_1_Mal_GP_1", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "L_at_Amin_Mal_GP_1", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "L_at_Amax_Mal_GP_1", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "VonBert_K_Mal_GP_1", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "CV_young_Mal_GP_1", 1:n]),
+            as.numeric(x$pars[x$pars$Label == "CV_old_Mal_GP_1", 1:n]) )  
+
+out = as.data.frame(out)
+colnames(out) = modelnames
+rownames(out) = c("Total Likelihood",
+                  "Survey Likelihood",
+                  "Length Likelihood",
+                  "Age Likelihood",
+                  "Recruitment Likelihood",
+                  "Forecast Recruitment Likelihood",
+                  "Parameter Priors Likelihood",
+                  "log(R0)",
+                  "SB Virgin",
+                  "SB 2020",
+                  "Fraction Unfished 2021",
+                  "Total Yield - SPR 50",
+                  "Steepness",
+                  "Natural Mortality - Female",
+                  "Length at Amin - Female",
+                  "Length at Amax - Female",
+                  "Von Bert. k - Female",
+                  "CV young - Female",
+                  "CV old - Female",
+                  "Natural Mortality - Male",
+                  "Length at Amin - Male",
+                  "Length at Amax - Male",
+                  "Von Bert. k - Male",
+                  "CV young - Male",
+                  "CV old - Male")
+
+
+write.csv(out, file = file.path(out.dir, paste0(base_model, "_sensitivities.csv")))
+
+t = table_format(x = out,
+      caption = 'Sensitivities relative to the base model.',
+      label = 'sensitivities',
+      longtable = TRUE,
+      font_size = 9,
+      digits = 3,
+      landscape = TRUE,
+      col_names = modelnames)
+
+kableExtra::save_kable(t,
+file = "C:/Assessments/2021/copper_rockfish_2021/write_up/wa/tex_tables/sensitivities.tex")
