514e94c331ce28d058d705c080652b01fc23761d
Chantel Wetzel <chantel.wetzel@noaa.gov>
2021-02-06 14:48:15 -0800

    update text

--- write_up/n_ca/_create_doc.R
+++ write_up/n_ca/_create_doc.R
@@ -5,7 +5,7 @@ library(sa4ss)
 
 
 # Specify the directory for the document
-setwd("C:/Assessments/2021/copper_rockfish/write_up/n_ca")
+setwd("C:/Assessments/2021/copper_rockfish_2021/write_up/n_ca")
 
 # Create the needed items to generate the "right" template that would be based on the inputs here:
 sa4ss::draft(authors = c("Chantel R. Wetzel", "Brian J. Langseth", "Jason M. Cope", "John E. Budrick"),
@@ -16,14 +16,31 @@ sa4ss::draft(authors = c("Chantel R. Wetzel", "Brian J. Langseth", "Jason M. Cop
   			 create_dir = FALSE,
   			 edit = FALSE)
 
-# Create a model Rdata object
-#read_model(mod_loc = "C:/Assessments/2021/squarespot_rockfish/models/0.01_init_model_updated_catches")
+#Create a model Rdata object
+sa4ss::read_model(
+          		  mod_loc = "C:/Assessments/2021/copper_rockfish_2021/models/ca_n_pt_c/9.0_base",
+				  create_plots = TRUE, 
+				  save_loc = file.path(getwd(), "tex_tables"),
+				  verbose = TRUE)
+
+load("00mod.Rdata")
+
+source("C:/Users/Chantel.Wetzel/Documents/GitHub/sa4ss/R/es_table_tex.R")
+SSexecutivesummary(replist = model, format = FALSE)
+es_table_tex(dir = mod_loc, 
+            save_loc = file.path(getwd(), "tex_tables"), 
+            csv_name = "table_labels.csv")
+
+# Read and create tex files for tables listed in "table" folder in the doc
+es_table_tex(dir = file.path(getwd(), 'tables'), 
+            save_loc = file.path(getwd(), "tex_tables"), 
+            csv_name = "all_tables.csv")
 
 if(file.exists("_main.Rmd")){
 	file.remove("_main.Rmd")
 }
 # Render the pdf
-bookdown::render_book("00a.Rmd", clean = FALSE)
+bookdown::render_book("00a.Rmd", clean=FALSE, output_dir = getwd())
 
 
 # Use to only render a specific section which can be quicker
