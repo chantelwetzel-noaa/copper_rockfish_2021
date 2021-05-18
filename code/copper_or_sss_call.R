5cd1d91576124d60acaa9d7b4f597800f5ca082f
Chantel Wetzel <chantel.wetzel@noaa.gov>
2021-05-18 07:32:13 -0700

    add sss call and summaries

--- code/copper_or_sss_call.R
+++ code/copper_or_sss_call.R
@@ -1,3 +1,4 @@
+
 ############################################################
 # EXAMPLE RUN SSS
 ############################################################
@@ -12,19 +13,19 @@ library(r4ss)
 
 # Assume a Beverton-Holt Stock Recruit relationship
 # define the directory
-Dir.in <-"C:/Assessments/2021/copper_rockfish_2021/models/sss/wa/7.7_base_depl_67"
-Dir.in <-"C:/Assessments/2021/copper_rockfish_2021/models/sss/wa/7.7_base_depl_44"
-
+Dir.in <-"C:/Assessments/2021/copper_rockfish_2021/models/sss/or/10.5_base_logistic_depl_47"
+Dir.in <-"C:/Assessments/2021/copper_rockfish_2021/models/sss/or/10.5_base_domed_depl_57"
+Dir.in <-"C:/Assessments/2021/copper_rockfish_2021/models/sss/or/10.5_base_depl_75"
 
 SSS.BH <- SSS(filepath= Dir.in, # location to run the model
-              file.name=c("2021_wa_copper.dat","2021_wa_copper.ctl"), # data and ctl file names
+              file.name=c("2021_or_copper.dat","2021_or_copper.ctl"), # data and ctl file names
               reps = 1000, # number of model runs
               seed.in = 19, # seed number
-              Dep.in = c(2, 0.44, 0.20), # c(distribution shape, mean, sd)
-              M.in = c(3, 0.108, 0.22, 3, 0.108, 0.22), # c(distribution shape for females, mean for females, sd for females, distribution shape for males, mean for males, sd for males)
+              Dep.in = c(2, 0.75, 0.20), # c(distribution shape, mean, sd)
+              M.in = c(3, 0.108, 0.24, 3, 0.108, 0.22), # c(distribution shape for females, mean for females, sd for females, distribution shape for males, mean for males, sd for males)
               h.in = c(1, 0.72, 0.15), # c(distribution, mean, sd)
               R_start = c(0, 3), # c( switch option, input value)
-              ts_yrs = c(1935, 2020),
+              ts_yrs = c(1927, 2022),
               sexes = TRUE)  # unique male and female M
 
 #######################################################################
@@ -34,9 +35,10 @@ SSS.BH <- SSS(filepath= Dir.in, # location to run the model
 #######################################################################
 library(HandyCode)
 library(sa4ss)
-dep = 67 #44 # 67
-base = paste0("7.7_base_depl_", dep)
-dir = "C:/Assessments/2021/copper_rockfish_2021/models/sss/wa"
+dep = 57 # 47
+selex = "domed" # "logistic"
+base = paste0("10.5_base_", selex, "_depl_", dep)
+dir = "C:/Assessments/2021/copper_rockfish_2021/models/sss/or"
 load(file.path(dir, base , "SSS_out.DMP"))
 load(file.path(dir, base, "SSSoutput.DMP"))
 out = SSS.out
@@ -51,52 +53,19 @@ depl.prior = out$Priors$Dep
 abc = apply(out$ABC, 2, quantile, c(0.025, 0.50, 0.975))
 ofl = apply(out$OFL, 2, quantile, c(0.025, 0.50, 0.975))
 
-library(ggplot2)
-library(ggthemes)
-library(gridExtra)
-
-
-
-p1 = ggplot(out$Priors, aes(x=M_f)) + 
-  geom_histogram(bins = 17, aes(y = stat(density)), color = 'black', fill = 'white') +
-  geom_vline(aes(xintercept = median(M_f)), linetype = 'dashed', size = 0.8, color = 'red') +
-  labs (x = "Natural Mortality (F)", y = "Density") + xlim(0, 0.25)
-
-p2 = ggplot(out$Priors, aes(x=M_m)) + 
-  geom_histogram(bins = 17, aes(y = stat(density)), color = 'black', fill = 'white') +
-  geom_vline(aes(xintercept = median(M_f)), linetype = 'dashed', size = 0.8, color = 'red') +
-  labs (x = "Natural Mortality (M)", y = "Density") + xlim(0, 0.25)
-
-p3 = ggplot(out$Priors, aes(x=h)) + 
-  geom_histogram(bins = 17, aes(y = stat(density)), color = 'black', fill = 'white') +
-  geom_vline(aes(xintercept = median(h)), linetype = 'dashed', size = 0.8, color = 'red') +
-  labs (x = "Steepness", y = "Density") + xlim(0.2, 1)
-
-p4 = ggplot(out$Priors, aes(x=Dep)) + 
-  geom_histogram(bins = 17, aes(y = stat(density)), color = 'black', fill = 'white') +
-  geom_vline(aes(xintercept = median(Dep)), linetype = 'dashed', size = 0.8, color = 'red') +
-  labs (x = "Fraction Unfished", y = "Density") + xlim(0, 1)
-
-pngfun(wd = file.path(dir, "_plots"), file = paste0(base, "_Priors_ggplot.png"), w = 7, h = 7, pt = 12)
-grid.arrange(p1, p2, p3, p4, nrow = 2, ncol = 2)
-dev.off()
-
-
-
-
-pngfun(wd = file.path(dir, "_plots"), file = paste0(base, "_Priors.png"), w = 7, h = 7, pt = 12)
+pngfun(wd = file.path(dir, "plots"), file = paste0(base, "_Priors.png"), w = 7, h = 7, pt = 12)
 par(mfrow = c(2,2))
-hist(m.f, xlab = "Natural Mortality (f)", main = "", xlim = c(0, 0.25))
+hist(m.f, xlab = "Natural Mortality (f)", main = "")
 abline(v = median(m.f), col = 'red', lwd = 3)
-hist(m.m, xlab = "Natural Mortality (m)", main = "", xlim = c(0, 0.25))
+hist(m.m, xlab = "Natural Mortality (m)", main = "")
 abline(v = median(m.m), col = 'red', lwd = 3)
-hist(h, xlab = "Steepness", main = "", xlim = c(0.20, 1))
+hist(h, xlab = "Steepness", main = "")
 abline(v = mean(h), col = 'red', lwd = 3)
-hist(depl.prior, xlab = "Fraction Unfished", main = "", xlim = c(0,1))
+hist(depl.prior, xlab = "Fraction Unfished", main = "")
 abline(v = mean(depl.prior), col = 'red', lwd = 3)
 dev.off()
 
-pngfun(wd = file.path(dir, "_plots"), file = paste0(base, "_quants.png"), w = 7, h = 7, pt = 12)
+pngfun(wd = file.path(dir, "plots"), file = paste0(base, "_quants.png"), w = 7, h = 7, pt = 12)
 par(mfrow = c(2,2))
 plot(colnames(ssb), ssb[2,], ylim = c(0, max(ssb)), type = 'l', lwd = 2, 
 	ylab = "Spawning output", xlab = "Year")
